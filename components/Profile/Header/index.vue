8bbfd01fdcae172798abe633ca52755756b1b4c2
Danil Kostin <d.kostin@yadro.com>
2023-11-23 21:05:14 +0300

    init prime vue, setting nuxt config

---
+++ components/Profile/Header/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="Profile header">
+    Profile header
+    <ProfileHeaderAvatar/>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ layouts/custom.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="CustomLayout">
+    <p>Custom layout</p>
+    <slot/>
+  </div>
+</template>
+<script setup>
+
+</script>
